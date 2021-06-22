0a79438b445114fcbb1144fa553cceec34c6990e
mnc345 <mnc345@stern.nyu.edu>
2021-06-22 12:23:36 -0400

    Updated liquor type input/response info; cocktail type commented out

--- app/cocktail_recipes.py
+++ app/cocktail_recipes.py
@@ -1,16 +1,43 @@
 import os
 from dotenv import load_dotenv
 import requests
+import json
 
 load_dotenv()
 
-COCKTAIL_API = os.getenv("COCKTAIL_API")
+# COCKTAIL_API = os.getenv("COCKTAIL_API")
 
-liquor = input("Please select a liquor type: ")
-print(f"SELECTED LIQUOR: '{liquor}'")
+def liquor_type():
+    liquor = input("Please select a liquor type: ")
+    print(f"SELECTED LIQUOR: '{liquor}'")
+    request_url = f"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={liquor}"
+    response = requests.get(request_url)
+    liquor_data = json.loads(response.text)
+#print(type(response))
+#print(response.text)
 
-request_url = f"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={liquor}"
+    for l in (liquor_data["drinks"]):
+        print(l["strDrink"], "\n")
 
-response = requests.get(request_url)
-print(type(response))
-print(response.text)
+liquor_type()
+
+
+# import webbrowser
+# def cocktails():
+#     cocktail_type = input("please select type of cocktail: ")
+#     request_url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={cocktail_type}"
+#     data = requests.get(request_url)
+#     tt = json.loads(data.text)
+    
+    
+#     for i in (tt["drinks"]):
+#         print(i["strDrink"], "\n")
+#         print(i["strInstructions"], "\n")
+        
+#         print(i["strIngredient1"])
+#         print(i["strIngredient2"])
+#         print(i["strIngredient3"])
+#         print(i["strIngredient4"])
+#         url = i["strDrinkThumb"]
+#         webbrowser.open(url)
+# cocktails()
