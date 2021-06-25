0a6d5a9caf0c2bf503469cb825038a48f5faeeae
mnc345 <mnc345@stern.nyu.edu>
2021-06-24 20:25:50 -0400

    fixed typo

--- app/cocktail_recipes.py
+++ app/cocktail_recipes.py
@@ -52,10 +52,10 @@ for index in range(1, 15):
     key = "strIngredient"+str(index)
     ingredient = drink[key]
     key = "strMeasure"+str(index)
-    measurment = drink[key]
+    measurement = drink[key]
     if (ingredient is not None):
-        if measurment is not None:
-            print(measurment, ingredient)
+        if measurement is not None:
+            print(measurement, ingredient)
         else:
             print(ingredient)
         
@@ -66,63 +66,62 @@ for index in range(1, 15):
 
 liquor
 
-
 ##Email the choice to user 
-
-
 
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+
 
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
 SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
 print("SendGrid Code",SENDGRID_API_KEY)
 
-client = SendGridAPIClient("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") #> <class 'sendgrid.sendgrid.SendGridAPIClient>
+client = SendGridAPIClient("SENDGRID_API_KEY") #> <class 'sendgrid.sendgrid.SendGridAPIClient>
 print("CLIENT:", type(client))
 
 subject = "Your Cocktail Recipe is HERE!!"
 
 USER_NAME = os.getenv("USER_NAME", default="Cocktail Lover")
+
 cocktail_choice = random_drink["strDrink"]
 drink = id_data["drinks"][0]
 instructions = drink["strInstructions"]
-ingrediant_one = drink["strIngredient1"]
-measurement_one= drink["strMeasure1"]
-ingrediant_2 = drink["strIngredient2"]
-measurement_2= drink["strMeasure2"]
-ingrediant_3 = drink["strIngredient3"]
-measurement_3= drink["strMeasure3"]
-ingrediant_4 = drink["strIngredient4"]
-measurement_4= drink["strMeasure4"]
-ingrediant_5 = drink["strIngredient5"]
-measurement_5= drink["strMeasure5"]
-ingrediant_6 = drink["strIngredient6"]
-measurement_6= drink["strMeasure6"]
-ingrediant_7 = drink["strIngredient7"]
-measurement_7= drink["strMeasure7"]
-ingrediant_8 = drink["strIngredient8"]
-measurement_8= drink["strMeasure8"]
-ingrediant_9 = drink["strIngredient9"]
-measurement_9= drink["strMeasure9"]
-ingrediant_10 = drink["strIngredient10"]
-measurement_10= drink["strMeasure10"]
-ingrediant_11 = drink["strIngredient11"]
-measurement_11= drink["strMeasure11"]
-ingrediant_12 = drink["strIngredient12"]
-measurement_12= drink["strMeasure12"]
-ingrediant_13 = drink["strIngredient13"]
-measurement_13= drink["strMeasure13"]
-ingrediant_14 = drink["strIngredient14"]
-measurement_14= drink["strMeasure14"]
-ingrediant_15 = drink["strIngredient15"]
-measurement_15= drink["strMeasure15"]
+ingredient_one = drink["strIngredient1"]
+measurement_one = drink["strMeasure1"]
+ingredient_2 = drink["strIngredient2"]
+measurement_2 = drink["strMeasure2"]
+ingredient_3 = drink["strIngredient3"]
+measurement_3 = drink["strMeasure3"]
+ingredient_4 = drink["strIngredient4"]
+measurement_4 = drink["strMeasure4"]
+ingredient_5 = drink["strIngredient5"]
+measurement_5 = drink["strMeasure5"]
+ingredient_6 = drink["strIngredient6"]
+measurement_6 = drink["strMeasure6"]
+ingredient_7 = drink["strIngredient7"]
+measurement_7 = drink["strMeasure7"]
+ingredient_8 = drink["strIngredient8"]
+measurement_8 = drink["strMeasure8"]
+ingredient_9 = drink["strIngredient9"]
+measurement_9 = drink["strMeasure9"]
+ingredient_10 = drink["strIngredient10"]
+measurement_10 = drink["strMeasure10"]
+ingredient_11 = drink["strIngredient11"]
+measurement_11 = drink["strMeasure11"]
+ingredient_12 = drink["strIngredient12"]
+measurement_12 = drink["strMeasure12"]
+ingredient_13 = drink["strIngredient13"]
+measurement_13 = drink["strMeasure13"]
+ingredient_14 = drink["strIngredient14"]
+measurement_14 = drink["strMeasure14"]
+ingredient_15 = drink["strIngredient15"]
+measurement_15 = drink["strMeasure15"]
 #this is where the template is not working 
-html_content = f"Hello {random}, you chose a cocktail with {liquor}!!, this is the name of your cocktail {cocktail_choice}!!,\n"
+html_content = f"Hello {USER_NAME}, You chose a cocktail with {liquor}!!, The name of your cocktail is {cocktail_choice}!!"
 html_content += f"instructions:{instructions}"
 html_content += f"{request_url}"
-html_content += f"These are the ingrediants you need:"
-html_content += f"{ingrediant_one}, {measurement_one},\n,{ingrediant_2}, {measurement_2}, \n, {ingrediant_3}, {measurement_3},\n, {ingrediant_4}, {measurement_4},\n,{ingrediant_5}, {measurement_5}, \n, {ingrediant_6}, {measurement_6}, \n , {ingrediant_7}, {measurement_7}, \n ,{ingrediant_8}, {measurement_8}, \n ,{ingrediant_9}, {measurement_9}, \n ,{ingrediant_10}, {measurement_10}, \n ,{ingrediant_11}, {measurement_11}, \n ,{ingrediant_12}, {measurement_12}, \n, {ingrediant_12}, {measurement_12}, \n, {ingrediant_13}, {measurement_13}, \n, {ingrediant_14}, {measurement_14}, \n, {ingrediant_15}, {measurement_15}, \n"
+html_content += f"These are the ingredients you need:"
+html_content += f"{ingredient_one}, {measurement_one},\n,{ingredient_2}, {measurement_2}, \n, {ingredient_3}, {measurement_3},\n, {ingredient_4}, {measurement_4},\n,{ingredient_5}, {measurement_5}, \n, {ingredient_6}, {measurement_6}, \n , {ingredient_7}, {measurement_7}, \n ,{ingredient_8}, {measurement_8}, \n ,{ingredient_9}, {measurement_9}, \n ,{ingredient_10}, {measurement_10}, \n ,{ingredient_11}, {measurement_11}, \n ,{ingredient_12}, {measurement_12}, \n, {ingredient_12}, {measurement_12}, \n, {ingredient_13}, {measurement_13}, \n, {ingredient_14}, {measurement_14}, \n, {ingredient_15}, {measurement_15}, \n"
 
 print("HTML:", html_content)
 message = Mail(from_email=SENDER_ADDRESS, to_emails=SENDER_ADDRESS, subject=subject, html_content=html_content)
@@ -140,3 +139,18 @@ except Exception as err:
    print(err) 
 
 
+
+print("HTML:", html_content)
+message = Mail(from_email=SENDER_ADDRESS, to_emails=SENDER_ADDRESS, subject=subject, html_content=html_content)
+
+try:
+    response = client.send(message)
+
+    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+    print(response.status_code) #> 202 indicates SUCCESS
+    print(response.body)
+    print(response.headers)
+
+except Exception as err:
+   print(type(err))
+   print(err) 
